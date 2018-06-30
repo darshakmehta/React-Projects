d6416b4775af49705e1d4caba14647e593cd2cda
tech-boy <darshak_mehta@live.com>
2018-06-30 01:00:02 -0400

    My Portfolio

---
+++ my-portfolio/src/app.js
@@ -0,0 +1,7 @@
+//install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(<p>This is my boilerlpate </p>,document.getElementById('app'));
